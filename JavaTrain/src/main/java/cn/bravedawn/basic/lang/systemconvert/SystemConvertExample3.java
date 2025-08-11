25b389d45b6a468f81746b71db2199697045924b
depers <dev_fengxiao@163.com>
2025-08-11 09:28:51 +0800

    2025-08-11

--- JavaTrain/src/main/java/cn/bravedawn/basic/lang/systemconvert/SystemConvertExample3.java
+++ JavaTrain/src/main/java/cn/bravedawn/basic/lang/systemconvert/SystemConvertExample3.java
@@ -15,6 +15,12 @@ import java.nio.ByteOrder;
 public class SystemConvertExample3 {
 
 
+    /**
+     * 将int转为十六进制字符串
+     * @param value
+     * @param bigEndian
+     * @return
+     */
     public static String intToHexWithByteBuffer(int value, boolean bigEndian) {
         ByteBuffer buffer = ByteBuffer.allocate(4);
         buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
@@ -28,6 +34,26 @@ public class SystemConvertExample3 {
     }
 
 
+    /**
+     * 将int转为byte数组
+     * @param value
+     * @return
+     */
+    public static byte[] intToBytesBigEndian(int value) {
+        ByteBuffer buffer = ByteBuffer.allocate(4);
+        buffer.order(ByteOrder.BIG_ENDIAN); // 设置为大端序（默认就是大端，这行可以省略）
+        buffer.putInt(value);
+        return buffer.array();
+    }
+
+
+    /**
+     * 将十六进制字符串转为int
+     * @param hexStr
+     * @param bigEndian
+     * @return
+     * @throws DecoderException
+     */
     public static int hexToIntWithByteBuffer(String hexStr, boolean bigEndian) throws DecoderException {
         byte[] bytes = Hex.decodeHex(hexStr.toCharArray());
         ByteBuffer buffer = ByteBuffer.wrap(bytes);
